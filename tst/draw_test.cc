cc9c795d47bc96f811933710b304141ddd0d6763
Melroy van den Berg <melroy@melroy.org>
2022-01-23 05:55:40 +0100

    Add test case to parser

--- tst/draw_test.cc
+++ tst/draw_test.cc
@@ -1,9 +1,18 @@
 #include "draw.h"
 #include "gtest/gtest.h"
+
 namespace
 {
   TEST(LibreWebTest, TestDrawA)
   {
+    // Given
+    // TODO: Use Google Mocking Middleware class
+    // Draw draw(middle);
+
+    // When
+    // draw.setText("Hello world");
+
+    // Then
   }
 
 } // namespace
--- tst/parser_test.cc
+++ tst/parser_test.cc
@@ -2,28 +2,31 @@
 #include "gtest/gtest.h"
 #include <cmark-gfm.h>
 #include <node.h>
+#include <string>
 namespace
 {
   TEST(LibreWebTest, TestContentParser)
   {
     // Given
+    std::string markdown = "Jaja";
     uint16_t expectedDocument = CMARK_NODE_DOCUMENT;
 
     // When
-    cmark_node* doc = Parser::parseContent("Jaja");
+    cmark_node* doc = Parser::parseContent(markdown);
 
     // Then
     ASSERT_EQ(doc->type, expectedDocument);
     cmark_node_free(doc);
   }
 
-  TEST(LibreWebTest, TestHTMLParser)
+  TEST(LibreWebTest, TestHTMLRender)
   {
     // Given
+    std::string markdown = "_Italic_ **BOLD** ~~strike~~";
     std::string expectedHtml = "<p><em>Italic</em> <strong>BOLD</strong> <del>strike</del></p>\n";
 
     // When
-    cmark_node* doc = Parser::parseContent("_Italic_ **BOLD** ~~strike~~");
+    cmark_node* doc = Parser::parseContent(markdown);
     std::string html = Parser::renderHTML(doc);
     cmark_node_free(doc);
 
@@ -31,4 +34,17 @@ namespace
     ASSERT_EQ(html, expectedHtml);
   }
 
+  TEST(LibreWebTest, TestMarkdownRender)
+  {
+    // Given
+    std::string markdown = "**HOLA**";
+
+    // When
+    cmark_node* doc = Parser::parseContent(markdown);
+    std::string markdownAgain = Parser::renderMarkdown(doc);
+    cmark_node_free(doc);
+
+    // Then
+    ASSERT_EQ(markdownAgain, markdown + "\n");
+  }
 } // namespace
