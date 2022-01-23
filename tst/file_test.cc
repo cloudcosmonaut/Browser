d19bcf549f69813645cfe3972690d556f7485d24
Melroy van den Berg <melroy@melroy.org>
2022-01-23 05:22:30 +0100

    Add 2 testcases

--- tst/file_test.cc
+++ tst/file_test.cc
@@ -3,7 +3,6 @@
 #include <string>
 namespace
 {
-
   TEST(LibreWebTest, TestGetFilename)
   {
     // Given
--- tst/parser_test.cc
+++ tst/parser_test.cc
@@ -1,15 +1,37 @@
 #include "md-parser.h"
 #include "gtest/gtest.h"
+#include <cmark-gfm.h>
+#include <node.h>
 namespace
 {
-
-  TEST(LibreWebTest, TestParserA)
+  TEST(LibreWebTest, TestContentParser)
   {
     // Given
+    uint16_t expectedDocument = CMARK_NODE_DOCUMENT;
 
     // When
+    cmark_node* doc = Parser::parseContent("Jaja");
 
     // Then
+    ASSERT_EQ(doc->type, expectedDocument);
+    cmark_node_free(doc);
   }
+
+
+  TEST(LibreWebTest, TestHTMLParser)
+  {
+    // Given
+    std::string expectedHtml = "<p><em>Italic</em> <strong>BOLD</strong> <del>strike</del></p>\n";
+
+    // When
+    cmark_node* doc = Parser::parseContent("_Italic_ **BOLD** ~~strike~~");
+    std::string html = Parser::renderHTML(doc);
+    cmark_node_free(doc);
+
+    // Then
+    ASSERT_EQ(html, expectedHtml);
+  }
+
+  
 
 } // namespace
