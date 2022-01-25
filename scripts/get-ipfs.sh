e6bbeb948dec7f7da75078c6498fce3f2955b0b2
Hindrik Bruinsma | Cloud Cosmonaut <hello@cloudcosmonaut.io>
2022-01-25 22:36:01 +0100

    Only download go-ips for current build

--- scripts/get-ipfs.sh
+++ scripts/get-ipfs.sh
@@ -10,17 +10,25 @@ IPFS_VERSION=0.11.0
 ##############################
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Download & untar/zip Go-IPFS
+OS=`uname`
+
 echo "INFO: Start downloading IPFS (version ${IPFS_VERSION})..."
-wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz" -O "$CURRENT_DIR/go-ipfs_linux.tar.gz"
-wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_windows-amd64.zip" -O "$CURRENT_DIR/go-ipfs_windows.zip"
-
-# Extract on root level of the git repo
-echo "INFO: Extracting Go IPFS..."
-tar -xzf "$CURRENT_DIR/go-ipfs_linux.tar.gz" -C "$CURRENT_DIR/../"
-unzip -q -o "$CURRENT_DIR/go-ipfs_windows.zip" -d "$CURRENT_DIR/../"
-
-# Clean-up
-echo "INFO: Clean up"
-rm -rf "$CURRENT_DIR/go-ipfs_linux.tar.gz"
-rm -rf "$CURRENT_DIR/go-ipfs_windows.zip"
+if [ "$OS" == "Linux" ]; then
+    echo "Downloading IPFS binaries for Linux"
+    wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz" -O "$CURRENT_DIR/go-ipfs_linux.tar.gz"
+    echo "Extracting IPFS binaries for Linux"
+    tar -xzf "$CURRENT_DIR/go-ipfs_linux.tar.gz" -C "$CURRENT_DIR/../"
+    rm "$CURRENT_DIR/go-ipfs_linux.tar.gz"
+elif [ "$OS" == "Darwin" ]; then
+    echo "Downloading IPFS binaries for MacOS"
+    wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_darwin-amd64.tar.gz" -O "$CURRENT_DIR/go-ipfs_darwin-amd64.tar.gz"
+    echo "Extracting IPFS binaries for MacOS"
+    tar -xzf "$CURRENT_DIR/go-ipfs_darwin-amd64.tar.gz" -C "$CURRENT_DIR/../"
+    rm "$CURRENT_DIR/go-ipfs_darwin-amd64.tar.gz"
+else
+    echo "Downloading IPFS binaries for Windows"
+    wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_windows-amd64.zip" -O "$CURRENT_DIR/go-ipfs_windows.zip"
+    echo "Extracting IPFS binaries for Windows"
+    unzip go-ipfs_windows.zip -d "$CURRENT_DIR/../"
+    rm "$CURRENT_DIR/go-ipfs_windows.zip"
+fi
