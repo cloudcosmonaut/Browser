6a5803aa274ecd3b32d8533468b2b2523acf8f91
Hindrik Bruinsma | Cloud Cosmonaut <hello@cloudcosmonaut.io>
2022-02-01 08:17:27 +0100

    [WIP] Add Mac support

---
+++ Darwin.md
@@ -0,0 +1,13 @@
+
+Installed requirements:
+* cppcheck
+* ninja
+* cmake
+* clang-format
+* gtk+
+* gtkmm3
+* curl-openssl
+* doxygen (maybe make it optional?)
+
+brew tap LouisBrunner/valgrind
+brew install --HEAD LouisBrunner/valgrind/valgrind
--- scripts/get-ipfs.sh
+++ scripts/get-ipfs.sh
@@ -13,22 +13,23 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OS=`uname`
 
 echo "INFO: Start downloading IPFS (version ${IPFS_VERSION})..."
+
 if [ "$OS" == "Linux" ]; then
     echo "Downloading IPFS binaries for Linux"
     wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz" -O "$CURRENT_DIR/go-ipfs_linux.tar.gz"
     echo "Extracting IPFS binaries for Linux"
     tar -xzf "$CURRENT_DIR/go-ipfs_linux.tar.gz" -C "$CURRENT_DIR/../"
     rm "$CURRENT_DIR/go-ipfs_linux.tar.gz"
+
+    echo "Downloading IPFS binaries for Windows"
+    wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_windows-amd64.zip" -O "$CURRENT_DIR/go-ipfs_windows.zip"
+    echo "Extracting IPFS binaries for Windows"
+    unzip go-ipfs_windows.zip -d "$CURRENT_DIR/../"
+    rm "$CURRENT_DIR/go-ipfs_windows.zip"
 elif [ "$OS" == "Darwin" ]; then
     echo "Downloading IPFS binaries for MacOS"
     wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_darwin-amd64.tar.gz" -O "$CURRENT_DIR/go-ipfs_darwin-amd64.tar.gz"
     echo "Extracting IPFS binaries for MacOS"
     tar -xzf "$CURRENT_DIR/go-ipfs_darwin-amd64.tar.gz" -C "$CURRENT_DIR/../"
     rm "$CURRENT_DIR/go-ipfs_darwin-amd64.tar.gz"
-else
-    echo "Downloading IPFS binaries for Windows"
-    wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_windows-amd64.zip" -O "$CURRENT_DIR/go-ipfs_windows.zip"
-    echo "Extracting IPFS binaries for Windows"
-    unzip go-ipfs_windows.zip -d "$CURRENT_DIR/../"
-    rm "$CURRENT_DIR/go-ipfs_windows.zip"
 fi
